0a7ec0422c610c1a862420c1ccb0c3ef0f58d1b9
Kevin Mooney <kmooney517@gmail.com>
2016-01-30 16:28:17 -0500

    fixed crud routes with new url

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -24,28 +24,28 @@ function appController ($scope, $http, SERVER) {
 
 
   $scope.addContact = function() {
-    $http.post('/contactList', $scope.contact).success(function(res) {
+    $http.post(url+ '/contactList', $scope.contact).success(function(res) {
       console.log(res);
     });
     refresh();
   }
 
   $scope.removeContact = function(id) {
-    $http.delete('/contactList/' + id).success(function(res) {
+    $http.delete(url+ '/contactList/' + id).success(function(res) {
       refresh();
     });
   }
 
   $scope.editContact = function(id) {
     console.log(id);
-    $http.get('/contactList/' + id).success(function(res) {
+    $http.get(url+ '/contactList/' + id).success(function(res) {
       $scope.contact = res;
     });
   }
 
   $scope.updateContact = function () {
     console.log('hi', $scope.contact._id);
-    $http.put('/contactList/' + $scope.contact._id, $scope.contact).success(function(res) {
+    $http.put(url+ '/contactList/' + $scope.contact._id, $scope.contact).success(function(res) {
       console.log(res);
       refresh();
     });
