1147fcf3fd7f081f6908b4027173765e7f0597fc
Kevin Mooney <kmooney517@gmail.com>
2016-01-30 17:13:33 -0500

    changed routes on the controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,13 +1,21 @@
 var app = angular
-  .module('app', []);
+  .module('app', [])
+  .constant('SERVER', {
+    URL:'https://boiling-wildwood-85823.herokuapp.com',
+    CONFIG: {
+      headers: {}
+    }
+  });
 
 app.controller('appController', appController);
 
-function appController ($scope, $http) {
+function appController ($scope, $http, SERVER) {
+
+  let url = SERVER.URL + '/contactList/';
 
   refresh();
   function refresh() {
-    $http.get('/contactList').success(function(res) {
+    $http.get(url, SERVER.CONFIG).success(function(res) {
       console.log(res);
       $scope.contactList = res;
     });
@@ -16,28 +24,28 @@ function appController ($scope, $http) {
 
 
   $scope.addContact = function() {
-    $http.post('/contactList', $scope.contact).success(function(res) {
+    $http.post(url, $scope.contact, SERVER.CONFIG).success(function(res) {
       console.log(res);
     });
     refresh();
   }
 
   $scope.removeContact = function(id) {
-    $http.delete('/contactList/' + id).success(function(res) {
+    $http.delete(url + id, SERVER.CONFIG).success(function(res) {
       refresh();
     });
   }
 
   $scope.editContact = function(id) {
     console.log(id);
-    $http.get('/contactList/' + id).success(function(res) {
+    $http.get(url + id, SERVER.CONFIG).success(function(res) {
       $scope.contact = res;
     });
   }
 
   $scope.updateContact = function () {
     console.log('hi', $scope.contact._id);
-    $http.put('/contactList/' + $scope.contact._id, $scope.contact).success(function(res) {
+    $http.put(url + $scope.contact._id, $scope.contact, SERVER.CONFIG).success(function(res) {
       console.log(res);
       refresh();
     });
